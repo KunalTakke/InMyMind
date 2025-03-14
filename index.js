a32dc7c79ddedf3334075ca55400c7fa73feb28b
KunalTakke <kunal.technophilic@gmail.com>
2025-03-14 18:23:34 +0530

    Added Dynamic behavior using Partials

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
+// imports
 import express from "express";
 
+// global variables
 const app = express();
 const port = 3000;
-// index route
+
+// telling app to look for static files in public 
+app.use(express.static("public"));
+
+// index page or home page
 app.get("/",(req,res)=>{
-    res.send("Hello World, from imymind!");
+    res.render("index.ejs");
 });
+
+// for read blog
+app.get("/read",(req,res)=>{
+    res.render("read.ejs");
+});
+// for about page
+app.get("/about",(req,res)=>{
+    res.render("about.ejs");
+});
+// for viewing /write route
+app.get("/write",(req,res)=>{
+    res.render("write.ejs");
+});
+
+// for posting on write blog
+app.post("/write",(req,res)=>{
+    res.render("write.ejs");
+});
+
+
 
 app.listen(port,()=>{
     console.log(`Listening to ${port} ... `);
